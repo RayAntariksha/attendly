f39450ca34748cecec85d5cd5c0454788c86d6d9
RayAntariksha <antariksharay20@gmail.com>
2025-11-12 09:34:44 +0530

    Commit before i try to fix some bugs

--- src/timelogic.rs
+++ src/timelogic.rs
@@ -12,3 +12,9 @@ pub fn school_time(opening_time: i32, closing_time: i32) -> bool {
         return false;
     }
 }
+pub fn date() -> i32 {
+    let utc = Utc::now();
+    let ist = utc.with_timezone(&Kolkata).format("%d").to_string();
+    let ist: i32 = ist.trim().parse().expect("Failed to parse time as i32");
+    ist
+}
